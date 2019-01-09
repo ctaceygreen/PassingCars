49527a15e8938387cd4be51177668de367b3b352
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 13:09:03 +0000

    Add project files.

---
+++ PassingCars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        long currentEast = 0;
+        long totalPasses = 0;
+        foreach(var car in A)
+        {
+            if(car == 0)
+            {
+                currentEast++;
+            }
+            else
+            {
+                totalPasses += currentEast;
+            }
+        }
+
+        if(totalPasses > 1000000000)
+        {
+            return -1;
+        }
+        else
+        {
+            return (int)totalPasses;
+        }
+    }
+}
